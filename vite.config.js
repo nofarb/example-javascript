0aa28ea99122704e73a009518e14a365c23c6718
nofarb <nofarb@gmail.com>
2025-03-19 01:22:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json'],
+      provider: 'v8',  // Use the V8 coverage provider
+      reporters: ['text', 'json', 'html', 'lcov'],  // Added 'html' and 'lcov' for HTML report
+      reportDir: 'coverage',  // Specify the output directory for coverage reports
     },
   },
 })
