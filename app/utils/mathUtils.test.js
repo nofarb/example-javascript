43205906082600c31cf4e4db7a82fd59738c6f4a
nofarb <nofarb@gmail.com>
2025-06-13 01:52:44 -0500

    Update mathUtils.test.js

--- app/utils/mathUtils.test.js
+++ app/utils/mathUtils.test.js
@@ -6,9 +6,6 @@ test('square function', () => {
   expect(square(-3)).toBe(9);
 });
 
-test('cube function', () => {
-  expect(cube(2)).toBe(8);
-  expect(cube(-3)).toBe(-27);
-});
+
 
 // Intentionally leave factorial untested for now
