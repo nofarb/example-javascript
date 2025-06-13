21b3a03d9657117de075e13b3fe3bfc97174e740
nofarb <nofarb@gmail.com>
2025-06-12 19:37:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['app/**/*.test.js'],
+    coverage: {
+      provider: 'v8',
+      reporters: ['text', 'json', 'html', 'lcov'],
+      reportDir: 'coverage'
+    },
     reporters: [
       'default',
       ['junit', { outputFile: 'junit.xml' }]
